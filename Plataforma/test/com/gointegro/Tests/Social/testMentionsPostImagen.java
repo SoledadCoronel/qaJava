98b9cf99656badf3ff5bc600323cd1366d7441c7
ampei <gd.moreira@yahoo.com.ar>
2014-07-16 10:29:38 -0300

    Agregados test Social

--- Plataforma/test/com/gointegro/Tests/Social/testMentionsPostImagen.java
+++ Plataforma/test/com/gointegro/Tests/Social/testMentionsPostImagen.java
@@ -2,9 +2,24 @@ package com.gointegro.Tests.Social;
 
 import static org.junit.Assert.*;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class testMentionsPostImagen {
+	
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+
+	@Rule
+	public TestWatcher testWatcher = new TestWatcher() {
+		@Override
+		protected void starting(final Description description) {
+			logger.info(description.getMethodName());
+		}
+	};
 
 	@Test
 	public void test() {
