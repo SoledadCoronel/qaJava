4ebc22ba068ce0f4ae62c3cbb43c3ccc9d5513fc
marinatouceda <marinatouceda@gointegro.com>
2016-02-05 15:47:01 -0300

    Ad

--- Go5/Automation/src/test/java/go5/automation/signup/ForgotPasswordSpanishTest.java
+++ Go5/Automation/src/test/java/go5/automation/signup/ForgotPasswordSpanishTest.java
@@ -61,25 +61,34 @@ public class ForgotPasswordSpanishTest {
 	   driver.manage().timeouts().implicitlyWait(15,TimeUnit.SECONDS);
 	   String SpanishText2 = new String( driver.findElement(By.cssSelector(".signup .reject ")).getText());
 	   Reporter.log(SpanishText2);
-		 System.out.println(SpanishText2);
 		 Assert.assertEquals(SpanishText2,"El email ingresado no se encuentra registrado. Por favor vuelve a intentarlo");
 	
 	// Insertar un email valido y verificar texto que se envio la contraseña
+		
+		 
+		 // Funtion to obtain link in the page
+		 
 		 JavascriptExecutor js2 = (JavascriptExecutor) driver;
 		js2.executeScript("$(document).ajaxComplete(function( event, xhr, settings ) { $('.primary').after('<a class=\"forgot-pass-link\" href=\"/authentication/reset-password/'+$.parseJSON(xhr.responseText).data.id+'\">Forgot Link!</a>'); });");
 		
 		driver.findElement(By.id("signInIdentification")).clear();
 		Reporter.log("Insertar el email y  presionar el  button to resend password");
-		driver.findElement(By.id("signInIdentification")).sendKeys("marina.touceda@gointegro.com");
+		driver.findElement(By.id("signInIdentification")).sendKeys("forgotpassword@gointegro.com");
 	   driver.findElement(By.cssSelector(".primary")).click();
 	     driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
 	   String SpanishText = new String( driver.findElement(By.cssSelector(".signup h2")).getText());
-	   System.out.println(SpanishText);
-		 Assert.assertEquals(SpanishText,"Revisa tu email.");
+	   Reporter.log(SpanishText);
+	   Assert.assertEquals(SpanishText,"Revisa tu email.");
 		 
+		 // Ir a la pagina para recuperar la contraseña
 		 driver.findElement(By.cssSelector(".forgot-pass-link")).click();
 		 driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
-	
+		 driver.findElement(By.id("passwordIdentification")).sendKeys("12345");
+		 Reporter.log(driver.findElement(By.cssSelector(".reject span")).getText());
+		 driver.findElement(By.id("passwordIdentification")).sendKeys("Auto1234");
+		 driver.findElement(By.cssSelector(".primary")).click();
+		 driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		 Reporter.log( driver.findElement(By.cssSelector(".signup p")).getText());
 	}
 	
      }
