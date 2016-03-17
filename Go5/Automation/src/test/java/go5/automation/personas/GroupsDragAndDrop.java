4b94f8cc83e5432a9bf46694160cefa38994fda5
marinatouceda <marinatouceda@gointegro.com>
2016-03-17 15:42:27 -0300

    Adding Drag and Drop Groups at Browser Stack

--- Go5/Automation/src/test/java/go5/automation/personas/GroupsDragAndDrop.java
+++ Go5/Automation/src/test/java/go5/automation/personas/GroupsDragAndDrop.java
@@ -33,13 +33,7 @@ public class GroupsDragAndDrop extends CommonFunctions{
 
 	//private WebDriver driver;
 
-	String s = "It was the best of times, it was the worst of times,\n"
-	         + "it was the age of wisdom, it was the age of foolishness,\n"
-	         + "it was the epoch of belief, it was the epoch of incredulity,\n"
-	         + "it was the season of Light, it was the season of Darkness,\n"
-	         + "it was the spring of hope, it was the winter of despair,\n"
-	         + "we had everything before us, we had nothing before us";
-
+	
 		 @BeforeTest // call function to open the browser and login 
 		 public void setup () throws Exception{
 		   openSiteLogin();
@@ -56,7 +50,7 @@ public class GroupsDragAndDrop extends CommonFunctions{
 	
 		
 
-	public void addUserAdmin() throws InterruptedException, FileNotFoundException{
+	public void dragAndDrop() throws InterruptedException, FileNotFoundException{
 		
 	 
 		
@@ -85,36 +79,17 @@ public class GroupsDragAndDrop extends CommonFunctions{
 	  	         
 	  	      //  Drag and Drop
 	  	         
-	  	        
+	  	        //Cargando elementos
 	  	       WebElement element = driver.findElement(By.cssSelector(".groupsform fieldset:nth-child(2)"));
 	  	       WebElement target = driver.findElement(By.cssSelector(".groupsform fieldset:nth-child(3)"));
 	  	  
-	//Probando un js q te pone un cuadrado rojo
+	//Cargando el javascript
 	  	       
   	     JavascriptExecutor js = (JavascriptExecutor) driver;
 
-  	    
-  	     js.executeScript("arguments[0].style.border='3px solid red'", element);
+  	    	  
   	     
-  	   // Need to wrap the javascript file
-  	  
-  	     
-  	   /*  FileInputStream filescript= new FileInputStream("/home/marinatouceda/Documentos/qa/Go5/Automation/src/test/java/go5/automation/drag_and_drop_helper.js");
-  	   
-  	   InputStream inStreamObject = ((InputStream) filescript);
-  	 Scanner sc = new Scanner( inStreamObject );
-  	 String input = sc.nextLine();
-  	 System.out.println(input);
-  	     
-  	   
-       String js_TxtFile = ""; 
-           while (sc.hasNext()) {          
-               String[] s = sc.next().split("\r\n");   
-               for (int i = 0; i < s.length; i++) {
-                   js_TxtFile += s[i];
-                   js_TxtFile += " ";
-               }           
-           }*/
+  	 
   	    
   	    try {
   	    	 String jquerySimulator = new String(Files.readAllBytes(Paths.get("/home/marinatouceda/Documentos/qa/Go5/Automation/src/test/java/go5/automation/drag_and_drop_helper.js")));
