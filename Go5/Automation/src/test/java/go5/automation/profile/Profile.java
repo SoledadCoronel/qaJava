615b9db3eec0b27eca522aaf3d1d3fed05c3dbe5
Cristina Fernicola <Cristina Fernicola>
2016-04-18 12:25:05 -0300

    Creando Profile Page

--- Go5/Automation/src/test/java/go5/automation/profile/Profile.java
+++ Go5/Automation/src/test/java/go5/automation/profile/Profile.java
@@ -1,46 +1,26 @@
 package go5.automation.profile;
 
 
-import java.net.URL;
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
+import go5.automation.TestSuite;
+import go5.pageObjects.ProfilePage;
+
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
+
 import org.testng.Reporter;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Parameters;
+
 import org.testng.annotations.Test;
 
 
 
-public class Profile  {
+public class Profile extends TestSuite  {
 	
  private WebDriver driver;
 
- protected String irACerrarSesion= new String ("a[title='Cierra la sesión");
+ProfilePage profile = null;
 
- @BeforeClass
-  @Parameters(value={"browser","version","platform","url"})
-  public void setUp(String browser, String version, String platform,String url) throws Exception {
-    DesiredCapabilities capability = new DesiredCapabilities();
-    capability.setCapability("platform",platform);
-    capability.setCapability("browserName", browser);
-    capability.setCapability("browserVersion", version);
-    capability.setCapability("project", "GOIntegro");
-    capability.setCapability("build", "1.0");
-    capability.setCapability("debug", false);
-    capability.setCapability("name", "Profile");
-    driver = new RemoteWebDriver(
-    		 new URL("http://rdgointegro1:8EKsJe3iYdeXFrKc2Byt@hub.browserstack.com/wd/hub"),
-    	      capability);
-    driver.get(url);
-	 driver.manage().window().maximize();
-	 driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS); 
-  }  
 
  @AfterClass // call function to close browser 
 	
@@ -53,90 +33,44 @@ public class Profile  {
 	public void editProfile() throws Exception { 
 	
 		
-		 
-		 Random numero= new Random();
+		 profile = new ProfilePage(driver);
 		 
 		
 		
 		 
 		 
 		// Go to the user menu
-			driver.findElement(By.cssSelector(".applications .users .user")).click();
+		  this.goToMenuUsuario();
+		//	driver.findElement(By.cssSelector(".applications .users .user")).click();
 	
-			 Reporter.log("Ir a Perfil");
+			
 			//Ir a ver perfil
-			driver.findElement(By.cssSelector(".applications .users .subusers li a[title='Ir a ver perfil']")).click();
+		   profile.goToProfiles();
+		//	driver.findElement(By.cssSelector(".applications .users .subusers li a[title='Ir a ver perfil']")).click();
 			
 			
 			//Editar perfil
+		   
+		   profile.editarProfile();	
 			 
-			 Reporter.log("Editar perfil");
-			driver.findElement(By.cssSelector(".data h2 a")).click();
+	
 			
 			//Insertar datos laborales
-			Reporter.log("Editando datos laborales");
+		    profile.editarDatosLaborales();
 			
-			 driver.findElement(By.cssSelector(".labordata label:nth-child(2) input")).clear();
-		    driver.findElement(By.cssSelector(".labordata label:nth-child(2) input")).sendKeys("RandomId" + numero.nextInt());
-  	         
-	         driver.findElement(By.cssSelector(".labordata label:nth-child(3) input" )).sendKeys("10/10/10");
-	         driver.findElement(By.cssSelector(".labordata label:nth-child(4) select ")).click();
-	         
-	         driver.findElement(By.cssSelector(".labordata label:nth-child(4) select ")).sendKeys("full-time");
-	         
-	       
-	         //Reṕorta a 
-	         driver.findElement(By.cssSelector(".labordata label:nth-child(6) input")).clear();
-	         driver.findElement(By.cssSelector(".labordata label:nth-child(6) input")).sendKeys("Test Test");
-	         
 	         //Telefono
-	         driver.findElement(By.cssSelector(".labordata label:nth-child(7) input")).clear();
-	           driver.findElement(By.cssSelector(".labordata label:nth-child(7) input")).sendKeys("+541147511234");
-	          driver.findElement(By.cssSelector(".labordata label:nth-child(8) input")).clear();
-	          driver.findElement(By.cssSelector(".labordata label:nth-child(8) input")).sendKeys("+541147511234");
+		    profile.agregarTelefono();
+	        
 	         
-	        /*  //Direccion
-	          driver.findElement(By.cssSelector(".labordata label:nth-child(9) input")).clear();
-	          driver.findElement(By.cssSelector(".labordata label:nth-child(9) input")).sendKeys("Alv. Thomas 198");
-	         */ //Fin Datos laborales
-	          
+	      	          
 	          //Datos Personales
-	          
-	          Reporter.log("Editando datos personales");
-	            driver.findElement(By.cssSelector(".personaldata label:nth-child(2) select")).sendKeys("RUT");
-	            driver.findElement(By.cssSelector(".personaldata label:nth-child(3) input")).clear();
-	            driver.findElement(By.cssSelector(".personaldata label:nth-child(3) input")).sendKeys("asasddsdsadsads");
-	            driver.findElement(By.cssSelector(".personaldata label:nth-child(4) input")).sendKeys("11/11/11");
-	            driver.findElement(By.cssSelector(".personaldata label:nth-child(5) select")).click();
-	            driver.findElement(By.cssSelector(".personaldata label:nth-child(5) select")).sendKeys("female");
-	            driver.findElement(By.cssSelector(".personaldata label:nth-child(6) select")).sendKeys("single");
-	            driver.findElement(By.cssSelector(".personaldata label:nth-child(7) input")).clear();
-	            driver.findElement(By.cssSelector(".personaldata label:nth-child(7) input")).sendKeys("fakee5555mail@gointegro.com");
-	        //    driver.findElement(By.cssSelector(".personaldata label:nth-child(8) input")).clear();
-	           // driver.findElement(By.cssSelector(".personaldata label:nth-child(8) input")).sendKeys("+541147511234");
-	          //  driver.findElement(By.cssSelector(".personaldata label:nth-child(9) input")).clear();
-	          //  driver.findElement(By.cssSelector(".personaldata label:nth-child(9) input")).sendKeys("+541147511234");
-	            driver.findElement(By.cssSelector(".personaldata label:nth-child(10) input")).clear();
-	            driver.findElement(By.cssSelector(".personaldata label:nth-child(10) input")).sendKeys("Mi Casa 123");
+		    
+	         profile.agregarDatosPersonales(); 
+      
 	            
-	            
-	            //Redes Sociales
-	            Reporter.log("Editando redes  sociables");
-	            driver.findElement(By.cssSelector(".socialdata label:nth-child(2) input")).clear();
-	          //  driver.findElement(By.cssSelector(".socialdata label:nth-child(2) input")).sendKeys("invaild data");
-	           // Assert.assertEquals("El campo debe tener un formato de url valido", driver.findElement(By.cssSelector(".socialdata label:nth-child(2) span")).getText(), "Validacion del campo linkedin");
-	         //   driver.findElement(By.cssSelector(".socialdata label:nth-child(2) input")).clear();
-	            
-	            driver.findElement(By.cssSelector(".socialdata label:nth-child(2) input")).sendKeys("http://linkedin.com/in/username");
-	           
-	            driver.findElement(By.cssSelector(".socialdata label:nth-child(3) input")).clear();
-	          //  driver.findElement(By.cssSelector(".socialdata label:nth-child(3) input")).sendKeys("invaild data");
-	           // Assert.assertEquals("El campo debe tener un formato de url valido", driver.findElement(By.cssSelector(".socialdata label:nth-child(3) span")).getText(), "Validacion del campo twitter");
-	           // driver.findElement(By.cssSelector(".socialdata label:nth-child(3) input")).clear();
-	            driver.findElement(By.cssSelector(".socialdata label:nth-child(3) input")).sendKeys("http://twitter.com/username");
-	          //  driver.findElement(By.cssSelector(".socialdata label:nth-child(4) input")).clear();
-	         //   driver.findElement(By.cssSelector(".socialdata label:nth-child(4) input")).sendKeys("http://facebook.com/username"); 
+	 		profile.agregarRedesSociales();
 	 		
+	 		profile.grabarUsuario();
 	     
 	            // Grabar el nuevo usuario creado
 	            Reporter.log("Grabando perfil editado");
