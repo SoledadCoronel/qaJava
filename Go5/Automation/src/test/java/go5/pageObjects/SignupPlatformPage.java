73b0b87c8f641fed572c466274f7564a35946f72
Cristina Fernicola <Cristina Fernicola>
2016-05-30 17:21:05 -0300

    Adding things useless

--- Go5/Automation/src/test/java/go5/pageObjects/SignupPlatformPage.java
+++ Go5/Automation/src/test/java/go5/pageObjects/SignupPlatformPage.java
@@ -34,21 +34,26 @@ import org.testng.Reporter;
      //Set password in password textbox
   
      public void setCorporateEmail(String strcorporateemail){
+    	 Reporter.log("Ingresar un mail corporativo para crear una plataforma");
     	 driver.findElement(By.id(idCorporateEmail)).sendKeys(strcorporateemail);
            }
   
   public void setEspañol(){
+	  Reporter.log("Seleccionar lenguaje español");
 	   driver.findElement(By.id(idLenguajeEspañol)).click();
   }
   
     public void setPortuges(){
+    	Reporter.log("Seleccionar lenguaje Portuges");
     	driver.findElement(By.id(idLenguajePortuges)).click();
     }
     public void setIngles(){
+    	Reporter.log("Seleccionar lenguaje Ingles");
     	driver.findElement(By.id(idLenguajeEnglish)).click();
     }
     
     public void clickSubmitButton(){
+    	Reporter.log("Presionar submit button");
     	driver.findElement(By.id(idSubmitButton)).click();
     	    	
     }
@@ -71,6 +76,7 @@ import org.testng.Reporter;
     }
     
     public String  getToken(){
+    	Reporter.log("Obtener el link para crear plataforma");
     	String urlToken =new String(driver.findElement(By.id("create_platform_link")).getAttribute("href"));
 		//driver.findElement(By.id("create_platform_link")).click();
 		return  getToken();
@@ -79,6 +85,7 @@ import org.testng.Reporter;
 		
     }
     public void irACreatePlatform(){
+    	Reporter.log("Clickear en el link para ir a crear plataforma");
     	driver.findElement(By.id("create_platform_link")).click();
     }
     }
