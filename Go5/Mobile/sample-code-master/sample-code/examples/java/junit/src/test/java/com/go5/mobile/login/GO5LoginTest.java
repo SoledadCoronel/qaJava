81acf80bc1516e5284f7943c110cca54db3f608e
marinatouceda <marinatouceda@gointegro.com>
2016-06-27 12:41:46 -0300

    Login q anda

--- Go5/Mobile/sample-code-master/sample-code/examples/java/junit/src/test/java/com/go5/mobile/login/GO5LoginTest.java
+++ Go5/Mobile/sample-code-master/sample-code/examples/java/junit/src/test/java/com/go5/mobile/login/GO5LoginTest.java
@@ -40,7 +40,7 @@ import java.util.concurrent.TimeUnit;
 public class GO5LoginTest {
     private AppiumDriver<AndroidElement> driver;
 
-    @BeforeTest
+    @Before
     public void setUp() throws Exception {
         // set up appium
         //File classpathRoot = new File(System.getProperty("user.dir"));
@@ -49,20 +49,20 @@ public class GO5LoginTest {
         
     	DesiredCapabilities capabilities = new DesiredCapabilities();
               
-        capabilities.setCapability("platformName","Android");
+      //  capabilities.setCapability("platformName","Android");
         capabilities.setCapability("deviceName","Android Emulator");            
-        capabilities.setCapability("noReset", true);
-        capabilities.setCapability("fullReset", false);
+     //   capabilities.setCapability("noReset", true);
+      //  capabilities.setCapability("fullReset", false);
         capabilities.setCapability("platformVersion", "5.1.1");//  es el mobile OS version, si instalo api22 es 5.1.1
        // The app capability is not requires if you set appPackage and appActivity
-        capabilities.setCapability("app", "/home/marinatouceda/Documentos/android-sdk-linux/platform-tools/go5-tag0.10.14.apk");
+       capabilities.setCapability("app", "/home/marinatouceda/Documentos/android-sdk-linux/platform-tools/go5-tag0.11.12.apk");
         capabilities.setCapability("appPackage", "com.gointegro.go5mobile");
-        capabilities.setCapability("appActivity", "com.gointegro.go5mobile.MainActivity");
+      //  capabilities.setCapability("appActivity", "com.gointegro.go5mobile.MainActivity");
         driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
         
     }
 
-    @AfterTest
+    @After
     public void tearDown() throws Exception {
         driver.quit();
     }
@@ -70,7 +70,9 @@ public class GO5LoginTest {
     @Test
     public void login() throws InterruptedException {
         //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    	Thread.sleep(10000);
+    	System.out.print("Esperando 10 segundos");
+    	//Thread.sleep(10000);
+    	System.out.println("Haciendo cambio de contexto");
         Set<String> contextNames = driver.getContextHandles();
         
        for (String contextName : contextNames) {
@@ -78,12 +80,13 @@ public class GO5LoginTest {
             if (contextName.contains("WEBVIEW")){
                 driver.context(contextName);
             }
+           
 
         }
+       Thread.sleep(10000);
+      //  WebDriverWait wait = new WebDriverWait(driver, 20);
         
-        WebDriverWait wait = new WebDriverWait(driver, 20);
-        
-        wait.until(ExpectedConditions.elementToBeClickable(By.tagName("BUTTON")));
+     //   wait.until(ExpectedConditions.elementToBeClickable(By.tagName("BUTTON")));
       //  Thread.sleep(10000);
         List<AndroidElement> textFieldsList = driver.findElementsByTagName("INPUT");
         textFieldsList.get(0).sendKeys("marina.touceda@gointegro.com");
@@ -98,7 +101,7 @@ public class GO5LoginTest {
         button.click();
         
         Thread.sleep(3000);
-        wait.until(ExpectedConditions.elementToBeClickable(By.tagName("h1")));
+     //   wait.until(ExpectedConditions.elementToBeClickable(By.tagName("h1")));
 
         WebElement title = driver.findElement(By.tagName("h1"));
         assertEquals("Principal", title.getText());
